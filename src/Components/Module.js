091fe840eff11ce4f0565861c1a0c7332c54562f
Sreejith36 <sreejith8230@gmail.com>
2023-05-26 11:06:58 +0530

    Delete button and some styling added

--- src/Components/Module.js
+++ src/Components/Module.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Module extends Component {
     state = {
@@ -6,9 +6,11 @@ export default class Module extends Component {
         items: []
     }
     handler = (event) => {
+
         this.setState({
             input: event.target.value
         });
+
     };
 
     storeItem = (event) => {
@@ -21,6 +23,16 @@ export default class Module extends Component {
         })
 
     };
+
+    deleteItem = (key) => {
+        console.log(key);
+        const allItems = this.state.items;
+        allItems.splice(key, 1);
+        this.setState({
+            items: allItems
+        })
+    };
+
     render() {
         const { input, items } = this.state
         console.log(items)
@@ -30,10 +42,13 @@ export default class Module extends Component {
 
                     <h1>Todo List</h1>
                     <form onSubmit={this.storeItem}>
-                        <input type='text' onChange={this.handler}
-                            value={input}
-                            placeholder='Enter Items' ></input>
-                        <button>Add</button>
+                        <div className='text-input' >
+
+                            <input type='text' onChange={this.handler}
+                                value={input}
+                                placeholder='Enter Items' ></input>
+                            <button>Add</button>
+                        </div>
                     </form>
                 </div>
                 <div className='list-output'>
@@ -41,8 +56,17 @@ export default class Module extends Component {
 
                         (data, index) => {
                             return (
+                                <div>
 
-                                <p key={index}>{data}</p>
+                                    <p key={index}>{data}
+                                        <i class="fa-regular fa-trash-can"
+                                            onClick={
+                                                () => { this.deleteItem(index) }
+                                            } ></i>
+                                    </p>
+
+                                </div>
+
                             )
                         })}
 
